b23e64585578c58da2f198040dc48904001f3875
rascasse-monaco <mktwkhr@gmail.com>
2021-05-17 00:32:54 +0900

    再アップ

---
+++ time-handling-util.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * conuntを時間に変換し十の位が0の場合は0を挿入する
+ * @param {Number} 1/10秒の数を入れる。
+ * @return {Number}
+ */
+function countToTime(count) {
+  return {
+    sec: Math.floor(count / 10 % 60).toString().padStart(2, '0'),//分を返す60秒になったら0秒にする
+    min: Math.floor(count / (60*10) % 60).toString().padStart(2, '0'),//60秒毎に分を返す60分になったら0分にする
+    hour: Math.floor(count / (60*60*10) % 24).toString().padStart(2, '0')//60分に時間を返す24時間になったら00時間にする
+  }
+}
+
+export default countToTime ;
