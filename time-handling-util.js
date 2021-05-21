c45483968c1c7611df183273ccd938d798a2ddc6
rascasse-monaco <mktwkhr@gmail.com>
2021-05-21 21:53:35 +0900

    コード最適化

--- time-handling-util.js
+++ time-handling-util.js
@@ -1,4 +1,8 @@
 'use strict';
+// 値を受け取って値を文字列にしてかえす。返す際に一桁の場合は十の位に0を入れる。
+function culcTime(value) {
+  return Math.floor(value).toString().padStart(2, '0');
+}
 /**
  * conuntを時間に変換し十の位が0の場合は0を挿入する
  * @param {Number} count 1/10秒のカウント数を入れる。
@@ -6,9 +10,9 @@
  */
 function countToTime(count) {
   return {
-    sec: Math.floor(count / 10 % 60).toString().padStart(2, '0'),//分を返す60秒になったら0秒にする
-    min: Math.floor(count / (60 * 10) % 60).toString().padStart(2, '0'),//60秒毎に分を返す60分になったら0分にする
-    hour: Math.floor(count / (60 * 60 * 10) % 24).toString().padStart(2, '0')//60分に時間を返す24時間になったら00時間にする
+    sec: culcTime(count / 10 % 60),//分を返す60秒になったら0秒にする
+    min: culcTime(count / (60 * 10) % 60),//60秒毎に分を返す60分になったら0分にする
+    hour: culcTime(count / (60 * 60 * 10) % 24)//60分に時間を返す24時間になったら00時間にする
   }
 }
 
